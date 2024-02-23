09edeade97c991151223fdee50849ba85e8dbc61
Matteo Monchiero <matteo.monchiero@snowflake.com>
2024-02-23 22:05:23 +0000

    test

--- Hello.py
+++ Hello.py
@@ -28,23 +28,19 @@ def run():
 
     st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+ 
+    conn = st.connection("snowflake")
+    conn.cursor().execute('use database FREE_DATASET_GZTSZAS2KH9')
+    query = conn.query("""SELECT i.cik, i.company_name, r.period_start_date, r.period_end_date, r.measure_description, TO_NUMERIC(r.value) AS value
+    FROM cybersyn.sec_cik_index AS i
+    JOIN cybersyn.sec_report_attributes AS r ON (r.cik = i.cik)
+    WHERE i.sic_code_description = 'AIR TRANSPORTATION, SCHEDULED'
+      AND r.statement = 'Income Statement'
+      AND r.period_end_date = '2022-12-31'
+      AND r.covered_qtrs = 4
+      AND r.metadata IS NULL
+      AND r.measure_description IN ('Total operating revenues', 'Total operating revenue');""")
+    st.dataframe(query)
 
 
 if __name__ == "__main__":
